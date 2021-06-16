98be1beb63e2e6287b73b416ba94bb58ed397007
XuanZhang <xzhangxmu@gmail.com>
2021-06-16 12:06:04 +1000

    Update inject_err.sh

--- Inject_ErrorOnRawData/inject_err.sh
+++ Inject_ErrorOnRawData/inject_err.sh
@@ -29,4 +29,4 @@ awk '{if((NR%2)==1)print $1;else print $0}' $F > ./input.fq;
 awk '{if(NR%4!=0)ORS=" ";else ORS="\n"}1' input.fq | awk '{print $1 " " $(NF-2) " " $NF}' > ./id_read.txt;
 awk '{print $2}' ./id_read.txt |sort |uniq -c| sort -r -nk1 > ./expreLevel_cor.txt;
 
-./induce_err -s $S -c expreLevel_cor.txt -f id_read.txt -o $O;
+./inject_err -s $S -c expreLevel_cor.txt -f id_read.txt -o $O;
